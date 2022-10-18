fede21735436b609694a6b730a63f75fc53266a2
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-10-18 15:22:52 +0200

    can configure som vars from env

--- proj/settings.py
+++ proj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,14 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-i23iv5wu3=+5vvat@o=-dcb+l#qqpr3x3uzl*21cof!cu(h(hb"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-i23iv5wu3=+5vvat@o=-dcb+l#qqpr3x3uzl*21cof!cu(h(hb",
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "false") == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split((":"))
 
 
 # Application definition
