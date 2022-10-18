7da0f84342e94acc601bc3b7fb2a921686ca07d9
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-10-18 15:35:40 +0200

    oups

--- proj/settings.py
+++ proj/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "false") == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split((":"))
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(":")
 
 
 # Application definition
